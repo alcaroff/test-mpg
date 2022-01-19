20cc5d7a061ec3d781ab6d152968c5ea83528864
alcaroff <acaroff10@gmail.com>
2022-01-19 20:18:05 +0100

    server com

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,18 @@ import { leagueRoutes } from "./league/league.routes";
 import { teamRoutes } from "./team/team.routes";
 
 const init = async () => {
+  // start server
   const server = Hapi.server({
     port: 3001,
     host: "localhost",
   });
-
   await server.start();
+
+  // merge and init routes
   server.route([...leagueRoutes, ...teamRoutes]);
   console.log("Server running on %s", server.info.uri);
+
+  // connect to database
   await connectCouchbase();
 };
 
